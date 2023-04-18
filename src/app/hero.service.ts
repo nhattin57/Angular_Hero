8f25a14e2cb8844ba6abd83d5ef457e90cee90d3
Đào Nhật Tín <nhattin57@gmail.com>
2023-04-18 17:55:17 +0700

    add navigation

---
+++ src/app/hero.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  private heroesUrl = 'api/heroes'; //URL to web API
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+
+  constructor(
+    //private http: HttpClient,
+    private messageService: MessageService) { }
+ 
+  // /** GET heroes from the server */
+   getHeroes(): Observable<Hero[]> {
+      const heroes = of(HEROES);
+      this.messageService.add('HeroService: fetched heroes');
+      return heroes;
+     //return this.http.get<Hero[]>(this.heroesUrl)
+   }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id===id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`)
+    return of(hero);
+  }
+  /** Log a HeroService message with the MessageService */
+private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+}
+
+}
